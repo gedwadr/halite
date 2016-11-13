95b45b48e5a91984aec69cb1b62b7f46e2c7a63f
egregorius <gregorius.edwadr@gdplabs.id>
2016-11-13 23:24:45 +0700

    version 4

--- MyBot.py
+++ MyBot.py
@@ -4,7 +4,9 @@ from networking import *
 
 LIMIT_TO_MOVE = 30
 LIMIT_TO_FUSE = LIMIT_TO_MOVE / 2 + 1
+LIMIT_TO_BORDER = 125
 MAX_SIZE = 255
+MAX_DEPTH = 20
 
 myID, game_map = getInit()
 sendInit("ElsaBot")
@@ -29,7 +31,7 @@ def attack_enemy():
     for neighbor in CARDINALS:
         if game_map.getSite(current_location, neighbor).owner != myID:
             enemy_strength = game_map.getSite(current_location, neighbor).strength
-            if  enemy_strength < my_strength:
+            if enemy_strength < my_strength:
                 enemy_production = game_map.getSite(current_location, neighbor).production
                 if best_enemy_production < enemy_production:
                     attack_direction = neighbor
@@ -44,26 +46,63 @@ def could_fuse(my_strength, my_friend_strength):
     return True
 
 
-def move_now():
-    move_direction = STILL
-    best_production = 0
+def get_direction_by_angle(l1, l2):
+    dx = l2.x - l1.x
+    dy = l2.y - l1.y
 
+    if dx > game_map.width - dx:
+        dx -= game_map.width
+    elif -dx > game_map.width + dx:
+        dx += game_map.width
+
+    if dy > game_map.height - dy:
+        dy -= game_map.height
+    elif -dy > game_map.height + dy:
+        dy += game_map.height
+
+    if (math.fabs(dy) > math.fabs(x)):
+        if dy > 0:
+            return NORTH
+        else:
+            return SOUTH
+    else:
+        if dx > 0:
+            return EAST
+        else:
+            return WEST
+
+
+def move_to_border(my_y, my_x):
+    shortest_distance = game_map.width
+    my_location = Location(my_x, my_y)
+    go_to = Location(0,0)
+    for y_temp in range(my_y - MAX_DEPTH, my_y + MAX_DEPTH):
+        for x_temp in range(my_x - MAX_DEPTH, my_x + MAX_DEPTH):
+            explore_location = Location(x_temp, y_temp)
+            if game_map.inBounds(explore_location):
+                if game_map.getDistance(my_location, explore_location) < shortest_distance:
+                    shortest_distance = game_map.getDistance(my_location, explore_location)
+                    go_to = explore_location
+
+    return get_direction_by_angle(my_location, go_to)
+
+
+def move_to_fuse():
+    for neighbor in CARDINALS:
+        if game_map.getSite(current_location).owner == myID:
+            if could_fuse(my_strength, game_map.getSite(current_location, neighbor).strength):
+                return neighbor
+
+
+def move_now():
     if my_strength < LIMIT_TO_MOVE:
         return STILL
 
-    # for neighbor in CARDINALS:
-    #     if game_map.getSite(current_location, neighbor).owner != myID:
-    #         if best_production < game_map.getSite(current_location, neighbor).production:
-    #             best_production = game_map.getSite(current_location, neighbor).production
-    #             move_direction = neighbor
+    if my_strength > LIMIT_TO_BORDER:
+        return move_to_border(y, x)
 
-    for neighbor in CARDINALS:
-        if could_fuse(my_strength, game_map.getSite(current_location, neighbor).strength) \
-                and last_move[current_location.x, current_location.y] != inverse_neighbor(neighbor):
-            move_direction = neighbor
-            break
+    return move_to_fuse()
 
-    return move_direction
 
 while True:
     moves = []
@@ -81,14 +120,8 @@ while True:
                 attack_enemy_direction = attack_enemy()
                 if attack_enemy_direction != STILL:
                     move = Move(current_location, attack_enemy_direction)
-                # else:
-                    # fuse_with_friend_direction = fuse_with_friend()
-                    # if fuse_with_friend_direction != STILL:
-                    #     moves.append(Move(current_location, fuse_with_friend_direction))
-                    #     move = True
                 if not move:
                     move = Move(current_location, move_now())
-
                 if move != None:
                     last_move[x,y] = move.direction
                     moves.append(move)
